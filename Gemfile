4d6a0fda6bb2269038c86fac2db94bd18c5b721b
Iain Mitchell <iainjmitchell@gmail.com>
2013-03-21 15:25:30 +0000

    'commit_message'

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'dashing'
 gem 'xml-simple'
 gem 'leankitkanban'
+gem 'git_repository'
---
+++ rakefile.rb
@@ -0,0 +1,26 @@
+task :default => [:dependencies, :unit_tests, :commit]
+
+task :dependencies do
+	sh "bundle install"
+end
+
+task :unit_tests do
+	Dir["./tests/**/*.rb"].each do | file |
+		sh "ruby #{file}"
+	end
+end
+
+task :commit do
+	require 'git_repository'
+	commit_message = ENV["m"] || 'no commit message'
+	git = GitRepository.new
+	git.add
+	git.commit(:message => 'commit_message')
+	git.push	
+end
+
+task :deploy do
+	require 'git_repository'
+	git = GitRepository.new(:remote => "heroku")
+	git.push
+end
