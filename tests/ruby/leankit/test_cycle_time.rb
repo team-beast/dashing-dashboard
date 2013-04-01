67fa4b367c762ab5b40b7bc68239577b0cfe4913
iainjmitchell <iainjmitchell@gmail.com>
2013-04-01 20:40:59 +0100

    dashboard_commit

--- tests/ruby/leankit/test_cycle_time.rb
+++ tests/ruby/leankit/test_cycle_time.rb
@@ -40,30 +40,91 @@ end
 BOARD_ID = 32404545
 
 class TestCycleTime < Test::Unit::TestCase
-	def test_how_the_fuck_leankit_api_works		
-		LeanKitKanban::Config.email    = "iain.mitchell@laterooms.com"
-		LeanKitKanban::Config.password = "Forest66"
-		LeanKitKanban::Config.url = "lrtest.leankit.com"
-
-    p LeanKit::CardCycleTimeFactory.new.create(1)
-
-
-		# File.open("output.txt", "w+") do |file|
-  #     file.puts(moved_history )
-  #   end
+	def test_kanban_calculate_cycle_time
+    p LeanKit::KanbanBoard.new(BOARD_ID, TestCycleRange.new).calculate_cycle_time
 	end
 end
 
+class TestCycleRange 
+  def initialize
+    @today = DateTime.now - 14
+  end
+
+  def is_satisfied_by(date_time)
+    date_time >= @today
+  end
+end
+
 module LeanKit
-  class CardCycleTimeFactory
-    def initialize
-      @datetime_from_leankit_datetime_factory = DateTimeFromLeanKitFactory.new
-      @cycle_time_calculator = CycleTimeCalculator.new
-      @card_cycle_history_factory = CardCycleHistoryFactory.new
+  class KanbanBoard
+    def initialize(board_id, within_cycle_range_specification = LeanKit::InLastNinetyDaysSpecification.new)
+      @within_cycle_range_specification = within_cycle_range_specification
+      @archieved_cards_respository = ArchivedCardsRepository.new(board_id)
+      @card_cycle_time_factory = LeanKit::CardCycleTimeFactory.new(board_id)
+      LeanKitKanban::Config.email    = "iain.mitchell@laterooms.com"
+      LeanKitKanban::Config.password = "Forest66"
+      LeanKitKanban::Config.url = "lrtest.leankit.com"
     end
 
-    def create(external_id)
-      card_cycle_history = @card_cycle_history_factory.create(external_id)
+    def calculate_cycle_time
+      card_count = 0
+      card_total_cycle_time = 0
+
+      archive = @archieved_cards_respository.get
+      archive.each do | archived_card | 
+        card_last_moved_date = DateTime.strptime(archived_card['LastMove'], '%d/%m/%Y')
+        if @within_cycle_range_specification.is_satisfied_by(card_last_moved_date)
+          card_cycle_time = @card_cycle_time_factory.create(archived_card["Id"])
+          if card_cycle_time.time_period != 0.0
+            card_count += 1
+            card_total_cycle_time +=  card_cycle_time.time_period
+          end
+        end
+      end
+      (card_count != 0 ) ? card_total_cycle_time / card_count : 0
+    end
+  end
+
+  class ArchivedCardsRepository
+    def initialize(board_id)
+      @board_id = board_id
+    end
+
+    def get
+      archive = get_archive_live_cards + get_archive_waste_cards
+    end
+
+    private
+    def get_archive_live_cards
+      LeanKitKanban::Archive.fetch(@board_id)[0][0]['ChildLanes'][0]['Lane']['Cards']
+    end
+
+    def get_archive_waste_cards
+      LeanKitKanban::Archive.fetch(@board_id)[0][0]['ChildLanes'][1]['Lane']['Cards']
+    end
+  end
+
+  class InLastNinetyDaysSpecification
+    def initialize
+      @ninety_days_ago = DateTime.now - 90
+    end
+
+    def is_satisfied_by(date_time)
+      date_time >= @ninety_days_ago
+    end
+  end
+
+
+  class CardCycleTimeFactory
+    def initialize(board_id)
+      @cycle_time_calculator = CycleTimeCalculator.new
+      @card_cycle_history_factory = CardCycleHistoryFactory.new(
+        CardTransitionHistoryRespository.new(
+          CardHistoryRespository.new(board_id)))
+    end
+
+    def create(card_id)
+      card_cycle_history = @card_cycle_history_factory.create(card_id)
       cycle_time = @cycle_time_calculator.calculate(
         card_cycle_history.start_cycle_date_time, 
         card_cycle_history.end_cycle_date_time)
@@ -75,16 +136,16 @@ module LeanKit
     START_CYCLE_LANE = 'Development: In Progress'
     END_CYCLE_LANE = 'Release: Live'
 
-    def initialize
-      @card_transistion_history_repository = CardTransitionHistoryRespository.new
+    def initialize(card_transistion_history_repository)
+      @card_transistion_history_repository = card_transistion_history_repository
       @datetime_from_leankit_datetime_factory = DateTimeFromLeanKitFactory.new
       @transition_date_time_mapper = TransitionDateTimeMapper.new(DateTimeFromLeanKitFactory.new())
     end
 
-    def create(external_id)
-      card_transistion_history = @card_transistion_history_repository.get(external_id)
-      start_cycle_date_time = @transition_date_time_mapper.map_from(card_transistion_history, START_CYCLE_LANE)
-      end_cycle_date_time = @transition_date_time_mapper.map_from(card_transistion_history, END_CYCLE_LANE)
+    def create(card_id)
+      card_transistion_history = @card_transistion_history_repository.get(card_id)
+      start_cycle_date_time = @transition_date_time_mapper.map_start_from(card_transistion_history)
+      end_cycle_date_time = @transition_date_time_mapper.map_end_from(card_transistion_history)
       CardCycleHistory.new(start_cycle_date_time: start_cycle_date_time, end_cycle_date_time:  end_cycle_date_time)
     end
   end
@@ -92,13 +153,22 @@ module LeanKit
   class TransitionDateTimeMapper
     DATE_TIME_KEY = 'DateTime'
     DESTINATION_LANE_KEY = 'ToLaneTitle'
+    START_CYCLE_LANE = 'Development: In Progress'
+    END_CYCLE_LANE = 'Release: Live'
 
     def initialize(date_time_adapter)
       @date_time_adapter = date_time_adapter
     end
 
-    def map_from(card_transistion_history, to_lane_title)
-      transition = get_transition_to(to_lane_title, card_transistion_history)
+    def map_start_from(card_transistion_history)
+      transition = get_transition_to(START_CYCLE_LANE, card_transistion_history)
+      transition = card_transistion_history[0] if transition.nil?
+      transition_date_time = @date_time_adapter.create(transition[DATE_TIME_KEY])
+    end
+
+    def map_end_from(card_transistion_history)
+      transition = get_transition_to(END_CYCLE_LANE, card_transistion_history)
+      transition = card_transistion_history[card_transistion_history.length-1] if transition.nil?
       transition_date_time = @date_time_adapter.create(transition[DATE_TIME_KEY])
     end
 
@@ -132,22 +202,24 @@ module LeanKit
   end
 
   class CardHistoryRespository
-    def get(external_id)
-      card = LeanKitKanban::Card.find_by_external_id(BOARD_ID, external_id)[0][0]
-      card_id = card["Id"]
-      LeanKitKanban::Card.get_card_history(BOARD_ID, card_id)[0]
+    def initialize(board_id)
+      @board_id = board_id
+    end
+
+    def get(card_id)
+      LeanKitKanban::Card.get_card_history(@board_id, card_id)[0]
     end
   end
 
   class CardTransitionHistoryRespository
     CARD_TRANSISITION_KEY_NAME = "FromLaneId" 
 
-    def initialize
-      @card_history_respository = CardHistoryRespository.new()
+    def initialize(card_history_respository)
+      @card_history_respository = card_history_respository
     end
 
-    def get(external_id)
-      card_history = @card_history_respository.get(external_id)
+    def get(card_id)
+      card_history = @card_history_respository.get(card_id)
       card_transistion_history = card_history.select { |card_history_item| card_history_item.key? CARD_TRANSISITION_KEY_NAME }
     end
   end
