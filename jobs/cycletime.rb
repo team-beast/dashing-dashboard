b7dff0a2f12c089de02a8529d778433d863b7bd9
Ben Flowers <ben.j.flowers@gmail.com>
2013-04-10 10:09:17 +0100

    'dashboard_commit'

--- jobs/cycletime.rb
+++ jobs/cycletime.rb
@@ -10,15 +10,15 @@ class CycleTimeScheduler
 
 	def start
 		SCHEDULER.every '10s', :first_in => 0 do
-  			LeanKit::KanbanBoard.new(BOARD_ID, self).calculate_cycle_time
+  		#	LeanKit::KanbanBoard.new(BOARD_ID, self).calculate_cycle_time
   		end  		
 	end
 
 	def show_cycle_time(cycle_time)
-  		rounded_cycle_time = cycle_time.round(2)
-  		@cycle_time_repository.add(rounded_cycle_time)
-  		points = @cycle_time_repository.get
-  		send_event('cycletime', points: points)
+  		# rounded_cycle_time = cycle_time.round(2)
+  		# @cycle_time_repository.add(rounded_cycle_time)
+  		# points = @cycle_time_repository.get
+  		send_event('cycletime', points: [{x: 23,y:34}])
 	end
 end
 
