44e3894f6edcea5a100562bb27ff47b31a1f560c
Iain Mitchell <iainjmitchell@gmail.com>
2013-04-30 14:21:39 +0100

    'dashboard_commit'

--- jobs/test_performance.rb
+++
@@ -1,8 +0,0 @@
-@points = []
-
-SCHEDULER.every '1m', :first_in => 0 do		
-	random_number = rand(30)
-	point = {:x => @points.length, :y => random_number}
-	@points.push(point)	
-	send_event('performance', points: @points )
-end
--- tests/ruby/leankit/test_cycle_time_repository_factory.rb
+++
@@ -1,7 +0,0 @@
-require 'test/unit'
-
-# class TestCycleTimeRepositoryFactory < Test::Unit::TestCase
-# 	# def test_
-		
-# end
-
