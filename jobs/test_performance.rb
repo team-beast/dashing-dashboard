f23949921a3636ab95f730dfb65933af9d5e49af
Iain Mitchell <iainjmitchell@gmail.com>
2013-04-30 09:18:41 +0100

    'dashboard_commit'

--- jobs/test_performance.rb
+++ jobs/test_performance.rb
@@ -1,7 +1,7 @@
 @points = []
 
 SCHEDULER.every '1m', :first_in => 0 do		
-	random_number = rand(0..30)
+	random_number = rand(30)
 	point = {:x => @points.length, :y => random_number}
 	@points.push(point)	
 	send_event('performance', points: @points )
