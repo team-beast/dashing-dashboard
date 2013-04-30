0a792ca00bf71a5d906b30c5888aea7c20079250
Iain Mitchell <iainjmitchell@gmail.com>
2013-04-30 13:48:28 +0100

    'dashboard_commit'

--- public/src/ServiceLayer.js
+++ public/src/ServiceLayer.js
@@ -1,6 +1,6 @@
 (function(){
 	this.ServiceLayer = function(jsonpRequestFactory){
-		var SERVICE_URI = "http://teambeastservices.herokuapp.com",
+		var SERVICE_URI = "http://teambeastservices.herokuapp.com/pipelines",
 			jsonpRequest = jsonpRequestFactory.create(SERVICE_URI);
 		function wakeUp(){
 			jsonpRequest.send();
