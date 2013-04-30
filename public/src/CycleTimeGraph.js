291d9c1820e282c9abb249d43d84e54842217260
Ben Flowers <ben.j.flowers@gmail.com>
2013-04-30 14:59:27 +0100

    'dashboard_commit'

---
+++ public/src/CycleTimeGraph.js
@@ -0,0 +1,49 @@
+var CycleTime = CycleTime || {};
+(function(module){
+	module.CycleTimeGraph = function(lineGraphFactory){
+			var lineGraph = lineGraphFactory.create(),
+				graphPoints = [],
+				maximumSelector = new MaximumSelector(render),
+				maximumYCalculator = new DoubleYCalulator(maximumSelector);
+
+			function update(points){
+				graphPoints = points;
+				maximumYCalculator.calculate(points);
+			}
+
+			function render(maxY){
+				lineGraph.render(maxY,graphPoints);
+			}
+
+			return{
+				update: update
+			};
+	};
+
+	var DoubleYCalulator = function(maximumSelector){
+		
+		function calculate(points){
+			heights = points.map(function(point){
+				return point.y * 2;
+			});
+			maximumSelector.select(heights);
+		}
+
+		return {
+			calculate: calculate
+		};
+	};
+
+	var MaximumSelector = function(render){
+		
+		function select(values){
+			maxY = Math.max.apply(null, values);
+			render(maxY)
+		}
+
+		return{
+			select: select
+		};
+	};
+
+})(CycleTime);
