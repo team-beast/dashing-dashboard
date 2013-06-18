a5a20ac75a8e53d0b2bfd642c3602078c8fc83ed
iainjmitchell <iainjmitchell@gmail.com>
2013-06-18 14:41:32 +0100

    lol catz

--- jobs/news.rb
+++ jobs/news.rb
@@ -5,6 +5,6 @@ require_relative '../src/news/BbcNews'
 SCHEDULER.every '15m', :first_in => 0 do |job|
 	headlines = @BBC_News.latest_headlines
 	headlines[4] = {:title => 'Travel company enters Administration', 
-		:description = 'The financial world is in shock as travel group TUI group enters administration'}
+		:description => 'The financial world is in shock as travel group TUI group enters administration'}
  	send_event('news', { :headlines => headlines})
 end
