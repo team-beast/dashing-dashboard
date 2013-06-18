ad0fcfe1ad0063967335a225b5be22ae3090886a
iainjmitchell <iainjmitchell@gmail.com>
2013-06-18 14:37:37 +0100

    lol catz

--- jobs/news.rb
+++ jobs/news.rb
@@ -4,7 +4,7 @@ require_relative '../src/news/BbcNews'
 
 SCHEDULER.every '15m', :first_in => 0 do |job|
 	headlines = @BBC_News.latest_headlines
-	headlines[2] = {:title => 'Concern at Savile Return', 
-		:description => 'There are reports that the spirit of Jimmy Savile has started infecting electrical devices in the UK'}
+	headlines[4] = {:title => 'Travel company enters Administration', 
+		:description = 'The financial world is in shock as travel group TUI group enters administration'}
  	send_event('news', { :headlines => headlines})
 end
